162ca21a562ee4a42a0c670c82b4a819fe998a2f
Joshua <Joshphilipose@gmail.com>
2018-05-26 17:20:01 -0400

    Got javascript to add textareas

---
+++ script.js
@@ -0,0 +1,11 @@
+function createMatrix() {
+    var cell = document.createElement("textarea");
+    document.getElementById('matrix').appendChild(cell);
+}
+
+// var form = document.getElementById("createMatrix");
+//
+// form.addEventListener("submit",function(e){
+//     document.getElementById('matrix').appendChild(cell);
+//     e.preventDefault();
+// });
